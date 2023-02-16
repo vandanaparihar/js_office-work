84135da3246925648c26ff9d7308de465f9fdfdd
vandanaparihar <95266805+vandanaparihar@users.noreply.github.com>
2023-02-16 16:06:19 +0530

    Add files via upload

---
+++ js opreator/js/script.js
@@ -0,0 +1,16 @@
+let x = 5;  //number
+//let y = '5';  // string
+//method
+//1. x=x+1;
+//2. x++;        //(operator) (5+1)=6
+//3. x--;
+
+let z= (x+5)/5;
+ 
+
+
+
+/*let a="hello";
+let b= "world";*/  
+
+console.log(z );
