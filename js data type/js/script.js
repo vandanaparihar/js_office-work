629c2f68b32c000fbc969f1916e24cbe53b1f06f
vandanaparihar <95266805+vandanaparihar@users.noreply.github.com>
2023-02-16 16:57:09 +0530

    Add files via upload

---
+++ js data type/js/script.js
@@ -0,0 +1,33 @@
+/*let x= "hel'lo";
+console.log(x);
+// 2 types of data type write
+let y='wor\'ld';
+console.log(y);*/
+
+//boolean data type: true/false
+
+/*let x=  2<3;
+console.log(x);*/
+
+//array:
+  /*let x=["sanaya,nehali,sejal,vandana"];
+   console.log(x);
+
+//object:
+    let man={name:"sanaya",age:20};
+    console.log(man);*/
+
+// undefined:
+/*let x;
+console.log(x);
+
+let y ="";//(string that is not undefined  )//
+console.log(y); */
+
+//type of :1 
+    let x= typeof "sanaya";
+    console.log(x);
+
+//2.
+let y="sanaya";
+alert(`hello ${y}`);
