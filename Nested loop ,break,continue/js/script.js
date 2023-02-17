23df8777eab2950257bd57115e220c70a8f60be3
vandanaparihar <95266805+vandanaparihar@users.noreply.github.com>
2023-02-17 15:41:56 +0530

    Add files via upload

---
+++ Nested loop ,break,continue/js/script.js
@@ -0,0 +1,24 @@
+for(let counter=1;counter<=10; counter++){
+    
+    if(counter==5){
+        break;
+    }
+    
+    
+    document.write(counter);
+    document.write('<br>');
+
+
+
+    for(let counter2 =1; counter2 <3; counter2++){
+        
+        if (counter ==3){
+            break;
+        }
+        
+        document.write('yashvi');
+        document.write('<br>');
+        
+    }
+
+} 
