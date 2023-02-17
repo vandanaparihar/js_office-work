ae038f379b39a457a314d4c76d33e07d35993517
vandanaparihar <95266805+vandanaparihar@users.noreply.github.com>
2023-02-17 13:11:40 +0530

    Add files via upload

---
+++ js loop/js/script.js
@@ -0,0 +1,26 @@
+
+for(let counter =1; counter <=10; counter++){
+    document.write('sanaya');
+}
+
+
+//let counter = 1;
+
+// while(counter <= 5){
+//     document.write('sanaya');
+//     counter++;
+// }
+ 
+ 
+
+// let counter =1;
+// let sum = 0;
+
+// while(counter <= 1000){
+//     if (counter % 2 ==0){
+//         sum = sum + counter;
+//     }
+//     counter++;
+
+// }
+// document.write(sum);
