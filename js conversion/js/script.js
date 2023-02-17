445b855e48e8d16eb3cb9ea9bf594901b05f21c7
vandanaparihar <95266805+vandanaparihar@users.noreply.github.com>
2023-02-17 17:16:00 +0530

    Add files via upload

---
+++ js conversion/js/script.js
@@ -0,0 +1,14 @@
+// let type= "5"*2 ;
+// console.log(type);
+// alert(typeof type);
+
+
+let type ="hello";
+console.log(typeof type);
+  
+let NewType  =String(type);
+console.log(NewType);
+console.log(typeof NewType);   
+
+
+
