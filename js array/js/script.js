7b1992c3c4d4bd47db98b8158aebafe1164fb584
vandanaparihar <95266805+vandanaparihar@users.noreply.github.com>
2023-02-21 15:41:57 +0530

    Add files via upload

---
+++ js array/js/script.js
@@ -0,0 +1,14 @@
+// let book1="Maths";
+// let book2="science";
+// let book3="eng";
+// let book4="computer";
+
+//book1 ="physics";
+// console.log(book3);
+
+
+let book = ["maths","science","eng","computer"];
+//let asset=[1,3,"maths",100];  
+// let book = new Array("maths","science","eng","computer");
+book[1]="physics";
+console.log(book);
