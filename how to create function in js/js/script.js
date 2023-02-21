ffbfcbfa84bd2dee0a1f8d742bd89341b17d3af4
vandanaparihar <95266805+vandanaparihar@users.noreply.github.com>
2023-02-21 17:29:47 +0530

    Add files via upload

---
+++ how to create function in js/js/script.js
@@ -0,0 +1,18 @@
+for(i=1;i<=10;i++){
+    document.write(`2* ${i} = ${2*1}`);
+    document.write("<br>");
+
+}
+
+
+function multiplicationtable(){
+    for(i=1;i<=10;i++){
+        document.write(`2* ${i} = ${2*1}`);
+        document.write("<br>");
+    
+    }
+}
+document.write("<br><br>");
+multiplicationtable();
+document.write("hello");
+multiplicationtable();
