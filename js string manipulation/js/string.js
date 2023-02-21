d75c94b02bb3cad60346c9ebe7732f7a620fdec0
vandanaparihar <95266805+vandanaparihar@users.noreply.github.com>
2023-02-21 12:34:04 +0530

    Add files via upload

---
+++ js string manipulation/js/string.js
@@ -0,0 +1,100 @@
+// let str ="sanaya \n singh";
+// //let greet =`HI ${str}`;
+// console.log(str[5]);
+
+// concatination
+
+
+// let str ="sanaya";
+
+// let str2="singh";
+
+// console.log(str +" "+ str2);
+
+
+// compair /case sensitive
+
+//  let str ="sanaya";
+// if("sanaya"== str){
+//     console.log("equal") 
+// }else{
+//     console.log("not equal") 
+// }
+
+//concat function:
+ 
+// let str ="sanaya";
+// let str2 = "singh";
+// let str3 = str.concat(" ", str2);
+// console.log(str3); 
+
+
+//substring:
+
+// let str = "this is substring. ";
+// let substring =str.substr(8,9);
+// console.log(substring);
+
+
+//position:(indexOf)
+
+// let str = "this is substring. ";
+// let position =str.indexOf('substring');
+// console.log(position);
+
+
+//position:(lastIndexOf)
+
+// let str = "this is substring. ";
+// let position =str.lastIndexOf('inn');
+// console.log(position);
+
+
+//removing extra space:
+
+// let str = " This is Substring.  ";
+// let trimedstr =str.trim();
+
+// console.log(str);
+// console.log (trimedstr); 
+
+
+//uppercase:
+
+// let str = " This is Substring.  ";
+// let trimedstr =str.toUpperCase ();
+
+// console.log(str);
+// console.log (trimedstr); 
+
+//'Lowercase:
+
+// let str = " This is Substring.  ";
+// let trimedstr =str.toLowerCase ();
+
+// console.log(str);
+// console.log (trimedstr); 
+
+//replace:
+
+// let str = " This is Substring.  ";
+// let trimedstr = str.replace ('This','that');
+// console.log(str);
+// console.log (trimedstr); 
+
+
+
+//include:
+
+// let str = " This is Substring.  ";
+// let trimedstr = str.includes('html');
+// let trimedstr1 = str.includes('Javascript');
+
+// console.log(str);
+// console.log (trimedstr); 
+// console.log (trimedstr1); 
+
+
+
+
+
