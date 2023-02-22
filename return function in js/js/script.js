6b8854cdee5ddb5ea3042224c0c3812aa5fa7a5b
vandanaparihar <95266805+vandanaparihar@users.noreply.github.com>
2023-02-22 16:30:10 +0530

    Add files via upload

---
+++ return function in js/js/script.js
@@ -0,0 +1,17 @@
+function add(a,b){
+    if(a>b){
+         return 1;
+    }
+    else if(b>a){
+        return -1;
+    }else{
+        return 0;
+    }
+    
+
+   
+  
+
+}
+let c = add(5,6);
+document.write(c); 
