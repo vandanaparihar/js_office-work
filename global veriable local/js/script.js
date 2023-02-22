bf8dd8ccad0aecc5cdc4ef035af8c8c8afc573c1
vandanaparihar <95266805+vandanaparihar@users.noreply.github.com>
2023-02-22 17:02:37 +0530

    Add files via upload

---
+++ global veriable local/js/script.js
@@ -0,0 +1,14 @@
+
+    let car = "audi"; //Global veriable
+
+    function add(){
+        let result =33; //local veriable
+        
+
+    }
+    function sub(){
+        let result = 50;
+        console.log(result);
+    }
+     add();
+     sub();
