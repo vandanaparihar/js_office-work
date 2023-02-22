845e0529e1e356e023d3e3c7512c70d3843602e7
vandanaparihar <95266805+vandanaparihar@users.noreply.github.com>
2023-02-22 11:28:47 +0530

    Add files via upload

---
+++ parameters and arguments in js/js/script.js
@@ -0,0 +1,26 @@
+ function MulTable(num){
+    for(let i=1; i<=12; i++){
+        document.write(`${num} * ${i} = ${num*1}`);
+        document.write("<br>");
+    }
+
+ document.write("<br><br>");
+}
+MulTable(12);
+document.write("<br>");
+MulTable(5);
+
+
+// take number input from the user
+        //const number = parseInt(prompt('Enter an integer: '));
+
+// take range input from the user
+        //const range = parseInt(prompt('Enter a range: '));
+
+//creating a multiplication table
+                    //for(let i = 1; i <= range; i++) {
+                    //const result = i * number;
+    //console.log(`${number} * ${i} = ${result}`);
+                    //document.write(`${number} * ${i} = ${result}`);
+                    //document.write("<br>");
+//}
