540b0f114a1ac20c09d79dd893e32abec5017482
vandanaparihar <95266805+vandanaparihar@users.noreply.github.com>
2023-02-27 13:11:52 +0530

    Add files via upload

---
+++ math object/js/script.js
@@ -0,0 +1,36 @@
+// math proprties:
+
+console.log(Math.PI);
+
+console.log(Math.E);
+
+
+//math object all function:
+var x= Math.round(4.2);
+console.log(x);
+
+var x = Math.ceil(4.2);
+console.log(x);
+
+var x= Math.floor(4.1);
+console.log(x);
+
+var x= Math.trunc(4.8);
+console.log(x);
+
+// power
+var x= Math.pow(2,3);
+console.log(x);
+
+//square root:
+var x= Math.sqrt(4);
+console.log(x);
+
+//max-min;
+var x= Math.min(4,22,32,4,3,);
+console.log(x);
+var x= Math.max(4,22,32,4,3);
+console.log(x);
+
+
+
