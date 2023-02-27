3738514d15ec358dc9e1d61c9d87dee664845d05
vandanaparihar <95266805+vandanaparihar@users.noreply.github.com>
2023-02-27 16:47:09 +0530

    Add files via upload

---
+++ Date object in js/js/script.js
@@ -0,0 +1,11 @@
+// 
+let x =  new Date("Octomber 13,2018 11:12:33");
+let y = new Date();
+if(x > y){
+   
+        console.log("x is past date");
+    }else if(x > y){
+        console.log("x is past date");
+    }else{
+        console.log("x is same date");
+    }
