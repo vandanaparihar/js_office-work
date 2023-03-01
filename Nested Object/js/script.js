2f44f29b914d72bf5f8a01c92ad1e0e4e07e3ce8
vandanaparihar <95266805+vandanaparihar@users.noreply.github.com>
2023-03-01 16:52:59 +0530

    Add files via upload

---
+++ Nested Object/js/script.js
@@ -0,0 +1,14 @@
+var user = {
+    id: 102,
+    email: "abc@gmail.com",
+    personalInfo:{
+        name:"abc",
+        address:{
+            street:"vhvh",
+            city: "rajkot",
+            country:"India "
+        }
+    }
+    
+};
+console.log(user.personalInfo.address.country);
